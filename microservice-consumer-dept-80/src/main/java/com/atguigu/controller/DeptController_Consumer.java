f6f2f631b40ec0b5194c102c7678fb4bfc59b662
qiupeng <1727873153@qq.com>
2019-10-24 16:04:01 +0800

    微服务部门提供者和消费者

---
+++ microservice-consumer-dept-80/src/main/java/com/atguigu/controller/DeptController_Consumer.java
@@ -0,0 +1,35 @@
+package com.atguigu.controller;
+
+import com.atguigu.entity.Deptentity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class DeptController_Consumer {
+    private static final String REST_URL_PREFIX="http://localhost:8887";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @PostMapping("/consumer/dept/add")
+    public boolean add(@RequestBody Deptentity deptentity)
+    {
+        return  restTemplate.postForObject(REST_URL_PREFIX+"/dept/add",deptentity,Boolean.class);
+
+    }
+    @GetMapping("/consumer/dept/findByid/{id}")
+    public Deptentity findByid(@PathVariable("id") Integer id)
+    {
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/findByid/"+id,Deptentity.class);
+    }
+
+    @GetMapping("/consumer/dept/findAll")
+    public List<Deptentity> findAll()
+    {
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/findAll",List.class);
+    }
+}
