c2c990adb31e449b61b07a4ba333d8f035221f86
qiupeng <1727873153@qq.com>
2019-10-31 16:57:07 +0800

    eureka 集群配置

---
+++ microservice-eureka-7002/src/main/java/com/aatgui/springcloud/EurekaService7002_App.java
@@ -0,0 +1,14 @@
+package com.aatgui.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaService7002_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaService7002_App.class,args);
+    }
+}
